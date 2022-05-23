d0adbe504fd158e3d00eee27ec046d278bf738e0
전가은 <jeonga-eun@jeonga-eun-ui-MacBookAir.local>
2022-05-23 21:42:52 +0900

    Inital commit

---
+++ src/main/java/site/dmarket/dmarketfrontend/DmarketFrontEndApplication.java
@@ -0,0 +1,13 @@
+package site.dmarket.dmarketfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmarketFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmarketFrontEndApplication.class, args);
+	}
+
+}
