62b47971671756e4a8ecb938438dba0c073a2c25
전가은 <jeonga-eun@jeonga-eun-ui-MacBookAir.local>
2022-05-26 15:11:01 +0900

    [gout] 회원가입 폼 진행 중...

--- src/main/java/site/dmarket/dmarketfrontend/web/TestController.java
+++ src/main/java/site/dmarket/dmarketfrontend/web/TestController.java
@@ -1,8 +1,13 @@
 package site.dmarket.dmarketfrontend.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "/join/joinForm";
+    }
 }
