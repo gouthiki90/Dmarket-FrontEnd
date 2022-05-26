0ec142e91d8e41b249d1d3d52fbfa5e79830265a
field <jsw_777@naver.com>
2022-05-26 16:05:38 +0900

    [정민] css 요소 추가 및 헤더 작업 중

---
+++ src/main/java/site/dmarket/dmarketfrontend/web/SecurityConfig.java
@@ -0,0 +1,16 @@
+package site.dmarket.dmarketfrontend.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // super.configure(http); 시큐리티 비활성화
+    }
+}
--- src/main/java/site/dmarket/dmarketfrontend/web/TestController.java
+++ src/main/java/site/dmarket/dmarketfrontend/web/TestController.java
@@ -1,8 +1,14 @@
 package site.dmarket.dmarketfrontend.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+
+    @GetMapping("/header")
+    public String header() {
+        return "/layout/header";
+    }
 
 }
